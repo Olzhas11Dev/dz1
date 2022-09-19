66f5d099e0a82977df3dc42ee4e442f4eeb177f3
olly <olzhas.kurmanbayev@centrapass.com>
2022-09-19 21:04:18 +1200

    Dz-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'dz1';
+  title = 'User list';
   typesOfShoes: string[] = [
     'Boots',
     'Clogs',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatListModule } from '@angular/material/list';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UsersListComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
